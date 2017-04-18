6b962bacb12ad984da9c77bc1e75df38af1ba174
baizhikang <baizhikang@hotmail.com>
2017-04-18 19:20:25 +0700

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :jobs
+  root 'jobs#index'
+
+  resources :jobs do
+  resources :resumes
+end
+
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+
+       resources :resumes
     end
   end
-
-  resources :jobs
-  resources :jobs do
-    resources :resumes
-  end
-
-  root 'jobs#index'
 end
