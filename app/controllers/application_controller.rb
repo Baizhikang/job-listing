c65ba535145b0f44754d0d23b65fa01b724e165f
baizhikang <baizhikang@hotmail.com>
2017-04-19 00:00:24 +0700

    better welcome index and admin/jobs/index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   def require_is_admin
     if !current_user.admin?
-      flash[:alert] = 'You are not admin'
+      flash[:alert] = '您没有管理者权限'
       redirect_to root_path
     end
   end
